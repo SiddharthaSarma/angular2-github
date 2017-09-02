a314b00e3cd0983101497956575b7916d8d52c0c
siddharthasarma <sidduinevitable@gmail.com>
2017-09-02 15:34:59 +0530

    Added main component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
+import { ApiService } from './api.service';
+import { TechnologyService } from './technology.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TechnologyComponent } from './technology/technology.component';
+import { ReposComponent } from './repos/repos.component';
+import { UsersComponent } from './users/users.component';
+import { routing } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TechnologyComponent,
+    ReposComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
-  providers: [],
+  providers: [TechnologyService, ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { TechnologyComponent } from './technology/technology.component';
+import { ReposComponent } from './repos/repos.component';
+import { UsersComponent } from './users/users.component';
+import {RouterModule, Routes} from '@angular/router';
+
+export const routes: Routes = [
+  {path: '', redirectTo: 'technologies', pathMatch: 'full'},
+  {path: 'technologies', component: TechnologyComponent},
+  {path: 'repos/:name', component: ReposComponent},
+  {path: 'users/:name', component: UsersComponent},
+];
+export const routing = RouterModule.forRoot(routes);
