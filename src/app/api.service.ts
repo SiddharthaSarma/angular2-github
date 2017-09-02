9c7f8a052ed5f122b1a6e129acf7806b430aac76
siddharthasarma <sidduinevitable@gmail.com>
2017-09-02 15:43:18 +0530

    Formatted the code

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import {Http, Headers, Response} from '@angular/http';
-import {Observable} from 'rxjs/Observable';
+import { Http, Headers, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 
@@ -8,17 +8,26 @@ import 'rxjs/add/operator/map';
 export class ApiService {
   apiUrl = `https://api.github.com/search/repositories?q=`;
   userDetailsUrl = 'https://api.github.com/users/';
-  constructor(private http: Http) { }
+  constructor(private http: Http) {}
 
   fetchReposBasedOnTechnology(technology: string) {
-    return this.http.get(`${this.apiUrl + technology}&language=${technology}&sort=stars&page=1&per_page=9`).map((response) => response.json());
+    return this.http
+      .get(
+        `${this.apiUrl +
+          technology}&language=${technology}&sort=stars&page=1&per_page=9`
+      )
+      .map(response => response.json());
   }
 
   fetchUserDetails(user: string) {
-    return this.http.get(this.userDetailsUrl + user).map((response) => response.json());
+    return this.http
+      .get(this.userDetailsUrl + user)
+      .map(response => response.json());
   }
 
   fetchUserRepos(user: string) {
-    return this.http.get(this.userDetailsUrl + user + '/repos').map((response) => response.json());
+    return this.http
+      .get(this.userDetailsUrl + user + '/repos')
+      .map(response => response.json());
   }
 }
